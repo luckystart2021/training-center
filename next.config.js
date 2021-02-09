f3d30b12df6680b3f5c311fe96ef9558752f5144
kevinNguyen97 <xaythixin@gmail.com>
2021-02-09 22:07:02 +0700

    commit dev branch

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withImage = require('next-images');
-
-const path = require('path')
-
-module.exports = withImage({
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-});
