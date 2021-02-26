71eb3554d8af414621fd542a1c7b92902290b341
DESKTOP-JMMNA3A\Dong <pdong15th06@gmail.com>
2021-02-26 21:54:43 +0700

    - edit ckeditor

--- src/components/ckeditor/index.js
+++ src/components/ckeditor/index.js
@@ -1,150 +1,59 @@
 import React, { Component } from "react";
+import CKEditor from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
+// https://ck
 class App extends Component {
-    render() {
-        return (
-            <div>dong ne</div>
-            // <CKEditor
-            //     data="<p>Hello from CKEditor 5!!</p>"
-            //     onInit={editor => console.log("Editor is ready to use!", editor)}
-            //     onChange={(event, editor) => {
-            //         console.log("Change", { event, editor });
-            //     }}
-            //     onBlur={(event, editor) => {
-            //         console.log("Blur.", { event, editor });
-            //     }}
-            //     onFocus={(event, editor) => {
-            //         console.log("Focus.", { event, editor });
-            //     }}
-            //     config={{
-            //         plugins: [
-            //             Essentials,
-            //             Paragraph,
-            //             Bold,
-            //             Italic,
-            //             Heading,
-            //             Indent,
-            //             IndentBlock,
-            //             Underline,
-            //             Strikethrough,
-            //             BlockQuote,
-            //             Font,
-            //             Alignment,
-            //             List,
-            //             Link,
-            //             MediaEmbed,
-            //             PasteFromOffice,
-            //             Image,
-            //             ImageStyle,
-            //             ImageToolbar,
-            //             ImageUpload,
-            //             ImageResize,
-            //             SimpleUploadAdapter,
-            //             Table,
-            //             TableToolbar,
-            //             TextTransformation
-            //         ],
-            //         toolbar: [
-            //             "heading",
-            //             "|",
-            //             "bold",
-            //             "italic",
-            //             "underline",
-            //             "strikethrough",
-            //             "|",
-            //             "fontSize",
-            //             "fontColor",
-            //             "fontBackgroundColor",
-            //             "|",
-            //             "alignment",
-            //             "outdent",
-            //             "indent",
-            //             "bulletedList",
-            //             "numberedList",
-            //             "blockQuote",
-            //             "|",
-            //             "link",
-            //             "insertTable",
-            //             "imageUpload",
-            //             "mediaEmbed",
-            //             "|",
-            //             "undo",
-            //             "redo"
-            //         ],
-            //         heading: {
-            //             options: [
-            //                 {
-            //                     model: "paragraph",
-            //                     view: "p",
-            //                     title: "본문",
-            //                     class: "ck-heading_paragraph"
-            //                 },
-            //                 {
-            //                     model: "heading1",
-            //                     view: "h1",
-            //                     title: "헤더1",
-            //                     class: "ck-heading_heading1"
-            //                 },
-            //                 {
-            //                     model: "heading2",
-            //                     view: "h2",
-            //                     title: "헤더2",
-            //                     class: "ck-heading_heading2"
-            //                 },
-            //                 {
-            //                     model: "heading3",
-            //                     view: "h3",
-            //                     title: "헤더3",
-            //                     class: "ck-heading_heading3"
-            //                 }
-            //             ]
-            //         },
-            //         fontSize: {
-            //             options: [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 25, 27, 29, 31, 33, 35]
-            //         },
-            //         alignment: {
-            //             options: ["justify", "left", "center", "right"]
-            //         },
-            //         table: {
-            //             contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"]
-            //         },
-            //         image: {
-            //             resizeUnit: "px",
-            //             toolbar: [
-            //                 "imageStyle:alignLeft",
-            //                 "imageStyle:full",
-            //                 "imageStyle:alignRight",
-            //                 "|",
-            //                 "imageTextAlternative"
-            //             ],
-            //             styles: ["full", "alignLeft", "alignRight"]
-            //         },
-            //         typing: {
-            //             transformations: {
-            //                 remove: [
-            //                     "enDash",
-            //                     "emDash",
-            //                     "oneHalf",
-            //                     "oneThird",
-            //                     "twoThirds",
-            //                     "oneForth",
-            //                     "threeQuarters"
-            //                 ]
-            //             }
-            //         },
-            //         simpleUpload: {
-            //             uploadUrl: "https://api.imgur.com/3/upload",
-            //             headers: {
-            //                 "X-CSRF-TOKEN": "CSFR-Token"
-            //                 // Authorization: 'Bearer <JSON Web Token>'
-            //             }
-            //         },
-            //         language: "ko"
-            //     }}
-            //     editor={ClassicEditor}
-            // />
-        );
-    }
+  render() {
+    return (
+      <>
+        <div>dong ne</div>
+        <CKEditor
+          editor={ClassicEditor}
+          // https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/frameworks/react.html#component-properties
+          // https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/configuration.html
+          config={{
+            // Pass the config for SimpleUploadAdapter
+            // https://ckeditor.com/docs/ckeditor5/latest/features/image-upload/simple-upload-adapter.html
+            simpleUpload: {
+              // The URL that the images are uploaded to.
+              uploadUrl: "https://api.imgur.com/3/upload",
+
+              // Enable the XMLHttpRequest.withCredentials property.
+              withCredentials: true,
+
+              // Headers sent along with the XMLHttpRequest to the upload server.
+              headers: {
+                "X-CSRF-TOKEN": "CSRF-Token",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Origin": "https://api.imgur.com",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+                "Access-Control-Allow-Headers": "accept, content-type",
+                "Access-Control-Max-Age": "1728000"
+                //Authorization: "Bearer <JSON Web Token>",
+              },
+            },
+          }}
+          data="<p>Hello from CKEditor 5!</p>"
+          onInit={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log("Editor is ready to use!", editor);
+            editor.plugins.get("TextTransformation").isEnabled = true;
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
