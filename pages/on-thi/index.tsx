73ffdf6155058d7194357c8e6665025fe3a61e4d
kevinNguyen97 <xaythixin@gmail.com>
2021-03-18 01:07:42 +0700

    add basic view user quiestions

--- pages/on-thi/index.tsx
+++ pages/on-thi/index.tsx
@@ -1,11 +1,24 @@
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import { Router, useRouter } from "next/router";
+import React, { FormEventHandler, useEffect, useState } from "react";
 import UserTemplate from "../../src/containers/UserTemplate";
-import { LicenseType, TestList } from "../../src/models/Question";
+import {
+  Answer,
+  AnswerChecked,
+  LicenseType,
+  Question,
+  Test,
+  TestList,
+} from "../../src/models/Question";
 import userRequestService from "../../src/services/userService/user.service";
 import style from "./style.module.scss";
 
 const namespace = "on thi";
+
+let countInterval = null;
+
+let count = 0;
+
+let isStartCount = false;
 
 const OnThi = (props) => {
   const title = "Thi sát hạch lý thuyết lái xe online";
@@ -16,9 +29,15 @@ const OnThi = (props) => {
 
   const [boDe, setBoDe] = useState<TestList>(new TestList());
 
-  const [boDeSelected, setBoDeSelected] = useState<number>(-1);
+  const [questions, setQuestions] = useState<Question[]>([]);
 
-  let loaiBoDeSelected = -1;
+  const [countTime, setCountTime] = useState<number>(count);
+
+  const [curentQuestions, setCurentQuestions] = useState<Question>(
+    new Question()
+  );
+
+  const [answersChecked, setAnswersChecked] = useState<AnswerChecked[]>([]);
 
   const getListLicensesType = () => {
     userRequestService.getLicenseType().then((license) => {
@@ -26,77 +45,213 @@ const OnThi = (props) => {
     });
   };
 
-  const getTestList = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    userRequestService.layDanhSachBoDe(e.target.value).then((res) => {
+  useEffect(() => {
+    handleSelectBoCauHoi(loaiBang[0]);
+    setAnswersChecked([]);
+  }, [loaiBang]);
+
+  useEffect(() => {
+    if (!countInterval && boDe) {
+      console.log("object");
+      count = boDe.timeSeconds;
+      countDown();
+    } else {
+      count = boDe.timeSeconds;
+    }
+    setAnswersChecked([]);
+  }, [questions, boDe]);
+
+  const getTestList = (idLoaiDe: number) => {
+    userRequestService.layDanhSachBoDe(idLoaiDe).then((res) => {
       setBoDe(new TestList(res.data));
     });
-
-    loaiBoDeSelected = Number(e.target.value);
-  };
-
-  const handleStart = () => {
-    router.push(`/on-thi/${boDeSelected}`);
   };
 
   useEffect(() => {
     getListLicensesType();
+    return () => {
+      clearInterval(countInterval);
+    };
   }, []);
+
+  const countDown = () => {
+    countInterval = setInterval(() => {
+      if (count) {
+        console.log(count);
+        count--;
+        setCountTime(count);
+      } else {
+        clearInterval(countInterval);
+      }
+    }, 1000);
+  };
+
+  const handleSelectBoCauHoi = (item: LicenseType) => {
+    router.push({
+      pathname: "/on-thi",
+      query: { bang: item?.licenseName, id: item?.id },
+    });
+
+    getTestList(item?.id);
+    setAnswersChecked([]);
+  };
+
+  const renderLoaiBang = () => {
+    return loaiBang.map((item) => (
+      <li className="nav-item mx-2" key={item.id}>
+        <a
+          className="nav-link text-white"
+          onClick={() => handleSelectBoCauHoi(item)}
+        >
+          Đề thi bằng {item.licenseName}
+        </a>
+      </li>
+    ));
+  };
+
+  const handleSelectBoDe = (item: Test) => {
+    userRequestService.getQuestions(item.id).then((res) => {
+      const data = res.data.map((item, index) => new Question(item, index));
+      setQuestions(data);
+      if (data.length) {
+        setCurentQuestions(data[0]);
+      }
+    });
+  };
+
+  const renderBoDe = () => {
+    return boDe?.suite.map((item) => (
+      <div className="d-inline">
+        <button
+          className="btn btn-success rounded m-2"
+          onClick={() => handleSelectBoDe(item)}
+        >
+          {item.name}
+        </button>
+      </div>
+    ));
+  };
+
+  const getTimneMinute = (sec: number): string => {
+    const minutes = Math.floor(sec / 60);
+    const seconds = sec - minutes * 60;
+    return sec ? `${minutes ? minutes : "00"}:${seconds ? seconds : "00"}` : "";
+  };
+
+  const renderCauHoi = () => {
+    return (
+      <div className="row">
+        <div className="col-sm-12 col-xl-4 p-3 border">
+          <div className="">Câu hỏi</div>
+          <div className="row">
+            {questions.map((cauHoi, i) => {
+              const index = answersChecked.findIndex(
+                (item) => item.id === cauHoi.id
+              );
+              return (
+                <div className="col-sm-2 col-xl-3 my-1" key={cauHoi.id}>
+                  <button
+                    className={`btn btn-${
+                      index === -1 ? "success" : "secondary"
+                    } rounded w-100`}
+                    onClick={() => {
+                      setCurentQuestions(cauHoi);
+                      if (i === 0) isStartCount = true;
+                    }}
+                  >
+                    {cauHoi.index + 1}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="">Thời gian còn lại: {getTimneMinute(countTime)}</div>
+        </div>
+        <div className="col-sm-12 col-xl-8 p-3 border">
+          <div className="">
+            <h3>Câu hỏi: {curentQuestions.index + 1}</h3>
+            <strong className="fs__17">{curentQuestions.question}</strong>
+            {curentQuestions.imgUrl ? (
+              <div className="d-flex justify-content-center">
+                <img src={curentQuestions.imgUrl} alt="" />
+              </div>
+            ) : (
+              ""
+            )}
+            <div className="order-details">
+              <div className="payment-box">
+                {renderCauTraLoi(curentQuestions.answerList)}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const handleOnCheck = (_index: number): void => {
+    const data = new AnswerChecked(curentQuestions.id, _index);
+
+    const index = answersChecked.findIndex((item) => item.id === data.id);
+    if (index !== -1) {
+      answersChecked[index] = data;
+    } else {
+      answersChecked.push(data);
+    }
+    setAnswersChecked([...answersChecked]);
+  };
+
+  // <input type="radio" id={`answer-${index}`} name="radio-group" />
+
+  const renderCauTraLoi = (answerList: Answer[]) => {
+    const itemChecked = answersChecked.find(
+      (item) => item.id === curentQuestions.id
+    );
+
+    return answerList.map((item, index) => {
+      return (
+        <div className="d-flex align-items-start" key={index}>
+          <input
+            className={style.input__select}
+            type="radio"
+            id={`answer-${curentQuestions.id}-${index}`}
+            name="radio-group"
+            checked={!!itemChecked && itemChecked.answerIndex === index}
+            onChange={() => handleOnCheck(index)}
+          />
+          <label
+            className="mb-0"
+            htmlFor={`answer-${curentQuestions.id}-${index}`}
+          >
+            {index} - {item.answer}
+          </label>
+        </div>
+      );
+    });
+  };
 
   return (
     <UserTemplate title={title} className={style.on__thi}>
-      <section className="contact-area ptb-110">
-        <div className="container text-center">
-          <h1>{title}</h1>
+      <nav className="navbar navbar-expand-lg">
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarSupportedContent"
+        >
+          <ul className=" navbar-nav bg-success rounded">
+            <li className="nav-item mx-2">
+              <a className="nav-link text-white">
+                Chọn Hạng thi <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            {renderLoaiBang()}
+          </ul>
         </div>
-        <div className="mx-auto">
-          <div className="d-flex justify-content-center align-items-center">
-            <div className={`input-group ${style.chon__bo__de}`}>
-              <select className="custom-select" onChange={getTestList}>
-                <option selected disabled>
-                  Chọn Loại Bằng
-                </option>
-                {loaiBang.map((item) => (
-                  <>
-                    <option key={item.id} value={item.id}>
-                      {item.licenseName}
-                    </option>
-                  </>
-                ))}
-              </select>
-            </div>
-
-            <div className={`input-group ${style.chon__bo__de}`}>
-              <select
-                className="custom-select"
-                id="SelectBoDe"
-                onChange={(e: any) => setBoDeSelected(Number(e.target.value))}
-              >
-                <option selected disabled>
-                  Chọn Bộ Đề
-                </option>
-                {boDe.suite?.map((item) => (
-                  <>
-                    <option key={item.id} value={item.id}>
-                      {item.name}
-                    </option>
-                  </>
-                ))}
-              </select>
-            </div>
-
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleStart}
-              disabled={boDeSelected === -1}
-            >
-              Bắt Đầu
-            </button>
-          </div>
-        </div>
-
-        <div className="container">{props.children}</div>
-      </section>
+      </nav>
+      <div className="container p-5">
+        <div className="">{renderBoDe()}</div>
+      </div>
+      <div className="container">{renderCauHoi()}</div>
     </UserTemplate>
   );
 };
